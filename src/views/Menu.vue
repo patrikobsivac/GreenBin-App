a5e534e2f360c270920f81dca930118457d75131
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2023-08-29 20:36:21 +0200

    Menu.vue Bugfix

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -3,61 +3,15 @@
     <div>
       <h1>Menu</h1>
       <br /><br />
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/report"
-        @click="changePage('page1')"
-        >Prijava Smeća</v-btn
-      >
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/route"
-        @click="changePage('page2')"
-        >Ruta smeća</v-btn
-      >
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/status"
-        @click="changePage('page3')"
-        >Status</v-btn
-      >
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/address"
-        @click="changePage('page4')"
+      <v-btn large class="mx-2" color="green" to="/report">Prijava Smeća</v-btn>
+      <v-btn large class="mx-2" color="green" to="/route">Ruta smeća</v-btn>
+      <v-btn large class="mx-2" color="green" to="/status">Status</v-btn>
+      <v-btn large class="mx-2" color="green" to="/address"
         >Pošalji Adresu</v-btn
       >
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/razvrstaj"
-        @click="changePage('page5')"
-        >Razvrstaj</v-btn
-      >
-      <v-btn
-        large
-        class="mx-2"
-        color="green"
-        a
-        href="/kontakti"
-        @click="changePage('page6')"
-        >Kontakti</v-btn
-      >
-      <v-btn large class="mx-2" color="white" a href="/" @click="logout()"
+      <v-btn large class="mx-2" color="green" to="/razvrstaj">Razvrstaj</v-btn>
+      <v-btn large class="mx-2" color="green" to="/kontakti">Kontakti</v-btn>
+      <v-btn large class="mx-2" color="white" @click="logout()"
         ><b>Logout</b></v-btn
       >
     </div>
